84ac6b0cf8a621233b0075f6b35decc96c0401c8
Daniyar Shulakov <dshulakov@gmail.com>
2020-07-06 16:31:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+require('dotenv').config()
 
 function App() {
+  const onClick = () => {
+    console.log(process.env.REACT_APP_API_URL);
+    axios.get(process.env.REACT_APP_API_URL)
+      .then( (resp) => console.log(resp.data)) 
+      .catch( (error) => console.log(error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
