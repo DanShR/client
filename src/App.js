faf1dc58ff5e498f9d2b44aaa069eb04f2696abb
Daniyar Shulakov <dshulakov@gmail.com>
2020-07-13 17:14:03 +0600

    api

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import axios from 'axios';
 ////require('dotenv').config()
 
 function App() {
-const URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3001' : 'http:178.170.221.190';
 
   const onClick = () => {
-    console.log(process.env.REACT_APP_API_URL);
-    console.log(process.env.NODE_ENV);
-    
     axios.get('/api')
       .then( (resp) => console.log(resp.data)) 
       .catch( (error) => console.log(error));
@@ -17,7 +13,7 @@ const URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3001' : '
 
   return (
     <div className="App">
-      <button onClick={onClick}>click me</button>
+      <button onClick={onClick}>click me please</button>
     </div>
   );
 }
